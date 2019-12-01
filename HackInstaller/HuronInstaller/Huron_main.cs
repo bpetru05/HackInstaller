06ee487e874148e753b24e81828c53d395567e9f
bpetru05 <bogdan.petrum@outlook.com>
2019-12-01 21:16:55 +0200

    Beginning of Hardware Scan

--- HackInstaller/HuronInstaller/Huron_main.cs
+++ HackInstaller/HuronInstaller/Huron_main.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Management;
 namespace HuronInstaller
 {
     class Huron_main
     {
+        private static void ScanForHardware()
+        {
+            string[] toScan ={"Win32_Processor","Win32_VideoController","Win32_NetworkAdapter","Win32_SoundDevice","Win32_Battery"
+            };
+            foreach(var option in toScan)
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + option);
+               
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
